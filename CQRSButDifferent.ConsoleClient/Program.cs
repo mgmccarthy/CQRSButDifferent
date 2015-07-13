51283933120dedc9dafeb0738dbd992cc5db26cc
indiecodemonkey <mike_mccarthy3@yahoo.com>
2015-07-12 21:19:09 -0400

    code cleanup

--- CQRSButDifferent.ConsoleClient/Program.cs
+++ CQRSButDifferent.ConsoleClient/Program.cs
@@ -17,18 +17,12 @@ namespace CQRSButDifferent.ConsoleClient
                 var random = new Random();
                 for (var i = 1; i <= int.MaxValue; i++)
                 {
-                    var productQuantity = GenerateProductQuantity(random);
+                    var productQuantity = random.Next(1, 6);
                     ServiceBus.Bus.Send(new PlaceOrder { OrderId = i, ProductId = 1, Quantity = productQuantity });
                     Console.WriteLine("An order with {0} of product 1 has been placed.", productQuantity);
                     Thread.Sleep(1000);
                 }
             }
-        }
-
-        static int GenerateProductQuantity(Random random)
-        {
-            // creates a number between 1 and 6
-            return random.Next(1, 6);
         }
     }
 }
--- CQRSButDifferent.Data/CqrsButDifferentContext.cs
+++ CQRSButDifferent.Data/CqrsButDifferentContext.cs
@@ -13,15 +13,6 @@ namespace CQRSButDifferent.Data
     }
 
     public class CqrsButDifferentContextInitializer : DropCreateDatabaseIfModelChanges<CqrsButDifferentContext>
-    //public class CqrsButDifferentContextInitializer : DropCreateDatabaseAlways<CqrsButDifferentContext>
     {
-        //forget seeding, let's resupply our vendor to start
-        //protected override void Seed(CqrsButDifferentContext context)
-        //{
-        //    for (var i = 1; i <= 1000; i++)
-        //    {
-        //        context.ProductQuantity.Add(new ProductQuantity {});
-        //    }
-        //}
     }
 }
