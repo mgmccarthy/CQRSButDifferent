fceebc17a30221669ce9785541e6031d2ca6702b
indiecodemonkey <mike_mccarthy3@yahoo.com>
2015-09-25 15:14:58 -0400

    updating readme and allowing publish of InsuffcientProductQuantityForOrder from PlaceOrderHandler (even though there is nothing subscribed to it)

--- CQRSButDifferent.OrderEndpoint/PlaceOrderHandler.cs
+++ CQRSButDifferent.OrderEndpoint/PlaceOrderHandler.cs
@@ -2,17 +2,25 @@
 using System.Linq;
 using CQRSButDifferent.Data;
 using CQRSButDifferent.Messages.Commands;
+using CQRSButDifferent.Messages.Events;
 using NServiceBus;
 
 namespace CQRSButDifferent.OrderEndpoint
 {
     public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
     {
+        private readonly IBus bus;
+
+        public PlaceOrderHandler(IBus bus)
+        {
+            this.bus = bus;
+        }
+
         public void Handle(PlaceOrder message)
         {
             using (var context = new CqrsButDifferentContext())
             {
-                //sum(delta) here to determine if there is enough quantity of the product in to place the order
+                //sum(delta) here to determine if there is enough quantity of the product to place the order
                 var productQuantity = context.ProductQuantity.Where(x => x.ProductId == message.ProductId).Sum(x => x.Delta);
 
                 if ((productQuantity < 20) && (productQuantity > 0))
@@ -23,14 +31,13 @@ namespace CQRSButDifferent.OrderEndpoint
                 
                 if (productQuantity < 0)
                 {
-                    //bus.Publish(new InsuffcientProductQuantityForOrder { OrderId = message.OrderId, ProductId = message.ProductId, Quantity = message.Quantity });
+                    bus.Publish(new InsuffcientProductQuantityForOrder { OrderId = message.OrderId, ProductId = message.ProductId, Quantity = message.Quantity });
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("We are out of product 1. Resupply the vendor now.");
                 }
                 else
                 {
                     //every time someone makes a purchase, we insert a new record with the negative of the quantity of the product they purchased
-                    //Math.Abs(message.Quantity) * (-1) negates the Quantity on PlaceOrder
                     var negativeQuantityForOrder = Math.Abs(message.Quantity) * (-1);
                     context.ProductQuantity.Add(new ProductQuantity { ProductId = message.ProductId, Delta = negativeQuantityForOrder, TimeStamp = DateTime.Now });
                     Console.ForegroundColor = ConsoleColor.Gray;
